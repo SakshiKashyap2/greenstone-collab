deadea3fc627fa9eb6f3e07818b494599dc462f3
Harsh Sharma <harshrajsharma.2003@gmail.com>
2023-06-14 02:45:53 +0530

    Added navigation and header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import "./Header.css"
 
-import Button from '@mui/material/Button';
 import Logo from '../../assets/images/Logo.png'
-const Header=()=>{
-    return(
+import CallIcon from '@mui/icons-material/Call';
+import Button from '@mui/material/Button';
+
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+
+const Header = () => {
+    return (
         <div className="head" >
             <div>
-                <img src={Logo} alt="Logo"/>
+                <img src={Logo} alt="Logo" />
             </div>
             <div>
-                <div>Phone</div>
-                <div>Email</div>
-                <div>Share</div>
+                <div><CallIcon />0064 7 850 1411</div>
+                <div>
+                    <Button variant="outlined" style={{
+                        color: "black",
+                        border: "2px solid",
+                        fontSize: "13px",
+                        fontWeight: "700",
+                        padding: "10px 18px 10px 18px"
+                    }}>
+                        Email Us
+                    </Button>
+                </div>
+                <div>
+                    <FacebookRoundedIcon style={{color:"rgb(66 103 178)"}} fontSize="large"/>
+                    <LinkedInIcon style={{color:"rgb(10, 102, 194)"}} fontSize="large"/>
+                    <WhatsAppIcon style={{color:"rgb(37 211 102)"}} fontSize="large"/>
+                </div>
             </div>
         </div>
     )
--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import "./Navigation.css"
 
+import UnfoldMoreDoubleIcon from '@mui/icons-material/UnfoldMoreDouble';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
 
-const Navigation = () =>{
-    return(
-        <div>
-            Navigation
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <div>
+                <a href="#">Home Page</a>
+                <a href="#">Find a Job</a>
+                <a href="#">Find Staff</a>
+                <a href="#">Services</a>
+                <a href="#">About</a>
+                <a href="#">News</a>
+                <a href="#">Contact</a>
+                <a href="#"><SearchIcon/></a>
+            </div>
+            <div>
+                <Button variant="outlined" style={{
+                    color: "rgb(0,62,11)",
+                    border: "2px solid white",
+                    backgroundColor: "white",
+                    fontSize: "13px",
+                    fontWeight: "700",
+                    padding: "10px 18px 10px 18px"
+                }}>
+                    GET IMMIGRATION ADVICE
+                </Button>
+            </div>
         </div>
     )
 }
